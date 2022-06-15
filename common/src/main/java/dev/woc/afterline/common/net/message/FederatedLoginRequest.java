0c08d329bb2d6636bae08bdde226e17321c3b8e6
andy <andy@delusion.org>
2022-06-14 17:11:31 -0700

    Add Login messages

---
+++ common/src/main/java/dev/woc/afterline/common/net/message/FederatedLoginRequest.java
@@ -0,0 +1,63 @@
+package dev.woc.afterline.common.net.message;
+
+import dev.woc.afterline.common.net.message.base.Message;
+import dev.woc.afterline.common.net.message.base.MessageID;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.Channel;
+
+import java.nio.charset.StandardCharsets;
+
+@MessageID(id = 2)
+public class FederatedLoginRequest extends Message {
+    private String username;
+    private String link;
+
+    public FederatedLoginRequest() {
+        super();
+    }
+
+    public FederatedLoginRequest(String uname) {
+        super();
+        username = uname;
+    }
+
+    public FederatedLoginRequest(String uname, String lnk) {
+        this(uname);
+        link = lnk;
+    }
+
+    @Override
+    protected void readFromBuffer(long msgSize, ByteBuf msgData) {
+        int ll = msgData.readInt();
+        link = msgData.readCharSequence(ll, StandardCharsets.UTF_8).toString();
+        int unl = msgData.readInt();
+        username = msgData.readCharSequence(unl, StandardCharsets.UTF_8).toString();
+    }
+
+    @Override
+    public ByteBuf writeBuffer(Channel ch, ByteBufAllocator alloc) {
+        if (link == null) {
+            link = "";
+        }
+
+        if (username == null) {
+            throw new RuntimeException("Username is required");
+        }
+
+        ByteBuf buf = createBuffer(alloc, getID(), link.length() + Integer.BYTES * 2 + username.length());
+        buf.writeInt(link.length());
+        buf.writeCharSequence(link, StandardCharsets.UTF_8);
+        buf.writeInt(username.length());
+        buf.writeCharSequence(username, StandardCharsets.UTF_8);
+        return buf;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ common/src/main/java/dev/woc/afterline/common/net/message/LoginComplete.java
@@ -0,0 +1,34 @@
+package dev.woc.afterline.common.net.message;
+
+import dev.woc.afterline.common.Utils;
+import dev.woc.afterline.common.net.message.base.MessageID;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.Channel;
+import dev.woc.afterline.common.net.message.base.Message;
+
+@MessageID(id = 3)
+public class LoginComplete extends Message {
+
+    private String realUsername;
+
+    public LoginComplete() {
+
+    }
+
+    @Override
+    protected void readFromBuffer(long msgSize, ByteBuf msgData) {
+        realUsername = Utils.readNextString(msgData);
+    }
+
+    @Override
+    public ByteBuf writeBuffer(Channel ch, ByteBufAllocator alloc) {
+        ByteBuf buf = createBuffer(alloc, getID(), Utils.calculateMessageLength(realUsername));
+        Utils.writeString(buf, realUsername);
+        return buf;
+    }
+
+    public String getUsername() {
+        return realUsername;
+    }
+}
